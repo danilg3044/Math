cb485a6882bd17da364334d4d6178894d5ed5943
Danil Gorelic <dgorelic@retalon.com>
2018-05-20 21:16:16 -0400

    a

--- build_image.bat
+++ build_image.bat
@@ -1,1 +1,1 @@
-"c:\Program Files\Docker Toolbox\docker" build -t mathapp:0.0.1 c:\Development\STS\Workspace\Math\MathWebApp\src
+"c:\Program Files\Docker Toolbox\docker" build -t mathapp:0.0.1 c:\Development\STS\Workspace\Math\MathWebApp
